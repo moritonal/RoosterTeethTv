ef4b594af9b78eff9eb9bc0a24e1b6e54c4342b2
Tom Bonner <tom+github@bonner.is>
2019-10-27 19:39:26 +0000

    Cleaned player

--- src/player.vue
+++ src/player.vue
@@ -1,19 +1,12 @@
 
 <template>
-	<div class="container-fluid p-3">
-		<div class="row">
-			<div class="embed-responsive embed-responsive-16by9">
-				<video
-					ref="videoPlayer"
-					class="embed-responsive-item video-js vjs-default-skin vjs-big-play-centered"
-					controls
-					preload="auto"
-					data-setup='{}'
-				>
-					<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that supports HTML5 video</p>
-				</video>
-			</div>
-		</div>
+	<div v-bind:class="{blur : !playerReader}">
+		<video
+			ref="videoPlayer"
+			class="embed-responsive-item video-js vjs-big-play-centered w-100 h-100"
+		>
+			<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that supports HTML5 video</p>
+		</video>
 	</div>
 </template>>
 
@@ -27,22 +20,25 @@ import RoosterTeethApi from "./RoosterTeethApi";
 export default Vue.extend({
 	data() {
 		return {
-			token: null,
-			player: null
+			player: null,
+			playerReader: false
 		};
 	},
 	methods: {
 		async playVideo(slug) {
-			let api = new RoosterTeethApi({
-				ClientId: process.env.CLIENT_ID,
-				Password: process.env.ROOSTERTEETH_PASSWORD,
-				Username: process.env.ROOSTERTEETH_USERNAME
-			});
+			let api = new RoosterTeethApi();
+			let vidsLink = (await api.getVideo(slug));
+			
+			console.log(vidsLink);
 
-			// let vidLink = await this.fetch(`https://red.bonner.is/roosterteeth/api/v1/watch/${slug}/videos`);
-			let vidLink = (await api.getVideo(slug)).data[0];
+			if (vidsLink.access == false) {
+				console.log(vidsLink);
+				console.warn("Test");
+				this.$router.push("/videos");
+				return;
+			}
 
-			console.log(vidLink);
+			let vidLink = vidsLink.data[0];
 
 			let url = vidLink.attributes.url;
 			let contentUUID = vidLink.attributes.content_uuid;
@@ -52,12 +48,11 @@ export default Vue.extend({
 			this.player = videojs(
 				this.$refs.videoPlayer,
 				{
-					autoplay: true,
 					controls: true
-				},
-				() => {
+				}, () => {
 					try {
-						if (this.player == null) return;
+						if (this.player == null)
+							return;
 
 						if (vidLink.attributes.media_type == "manifest") {
 							this.player.src({
@@ -66,7 +61,46 @@ export default Vue.extend({
 							});
 
 							this.player.ready(() => {
+
+								try {
+								this.player.play();
+								this.player.focus();
+
+								let keyDown = (event : KeyboardEvent) => {
+
+									switch (event.keyCode) {
+										case 37: // Left
+											let seekTimeBackwards = Math.max(0, this.player.currentTime() - 5);
+											this.player.currentTime(seekTimeBackwards);
+											event.preventDefault();
+											break;
+										case 39: // Right
+											let seekTimeForwards = Math.min(this.player.duration(), this.player.currentTime() + 5);
+											this.player.currentTime(seekTimeForwards);
+											event.preventDefault();
+											break;
+										case 38: // Up
+											break;
+										case 40: // Down
+											break
+										case 32: // Space
+											if (this.player.paused()) {
+												this.player.play();
+											} else {
+												this.player.pause();
+											}
+											break;
+										default:
+											console.warn("Unhandled key", event);
+											break;
+									}
+								}
+
+								this.player.on('keydown', keyDown);
+
+								// Setup loop to set current Time
 								this.player.setInterval(async () => {
+
 									if (!this.player.paused()) {
 										let currentTime = this.player.currentTime();
 
@@ -85,12 +119,20 @@ export default Vue.extend({
 										);
 									}
 								}, 5000);
+
+								// Set the current time
+								this.player.currentTime(position.value);
+
+								this.playerReader = true;
+								}
+								catch (ex) {
+									console.warn(ex);
+								}
 							});
+						} else {
+							alert(`Unsupported media-type: ${vidLink.attributes.media_type}`);
 						}
 
-						console.log(this.player);
-
-						this.player.currentTime(position.value);
 					} catch (err) {
 						console.warn("Cap", err);
 					}
@@ -120,6 +162,13 @@ $body-color: #e0e0e0;
 @import "node_modules/bootstrap/scss/bootstrap";
 @import "node_modules/bootstrap-vue/src/index.scss";
 
+div {
+	transition: all 1s;
+}
+
+.blur {
+	filter: blur(2px);
+}
 .card {
 	background-color: transparent;
 }
